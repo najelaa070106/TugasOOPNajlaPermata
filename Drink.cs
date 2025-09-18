043938ccf033d35de407e369b88cc628df73adb0
najelaa070106 <najlaaprmt@gmail.com>
2025-09-18 10:42:10 +0700

    Add files via upload

---
+++ Drink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TugasOOP
+{
+    public class Drink : MenuItem
+    {
+        public bool IsDingin { get; set; }
+
+        public Drink(string _namaMenu, double _harga, bool _isDingin)
+            : base(_namaMenu, _harga)
+        {
+            IsDingin = _isDingin;
+        }
+
+        public override void DisplayInfo()
+        {
+            string _tipe = IsDingin ? "Dingin" : "Panas";
+            Console.WriteLine(NamaMenu + (_tipe) + " - Rp " + Harga);
+        }
+    }
+}
---
+++ MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TugasOOP
+{
+    public class MenuItem
+    {
+        public string NamaMenu { get; set; }
+        public double Harga {  get; set; }
+
+        public MenuItem(string _namaMenu , double _harga)
+        {
+            NamaMenu = _namaMenu;
+            Harga = _harga;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine(NamaMenu + " - Rp " + Harga);
+        }
+    }
+}
