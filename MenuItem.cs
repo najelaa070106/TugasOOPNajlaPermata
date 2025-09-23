f04e283e39e5bece986a9da4f5cc59c4befc583b
najelaa070106 <najlaaprmt@gmail.com>
2025-09-23 22:24:54 +0700

    Update MenuItem.cs

--- MenuItem.cs
+++ MenuItem.cs
@@ -1,21 +1,63 @@
-﻿using System;
+using System;
 
 namespace TugasOOP
 {
+    // class
     public class MenuItem
     {
-        public string NamaMenu { get; set; }
-        public double Harga {  get; set; }
+        // encapsulation
+        private string NamaMenu;
+        private decimal Harga;
+        private int Stok;
 
-        public MenuItem(string _namaMenu , double _harga)
+        public string GetNamaMenu()
         {
-            NamaMenu = _namaMenu;
-            Harga = _harga;
+            return NamaMenu;
         }
 
+        public void SetNamaMenu(string newNamaMenu)
+        {
+            NamaMenu = newNamaMenu;
+        }
+
+        public decimal GetHarga()
+        {
+            return Harga;
+        }
+        public void SetHarga(decimal newHarga)
+        {
+            Harga = newHarga;
+        }
+        public int GetStok()
+        {
+            return Stok;
+        }
+
+        public void SetStok(int newStok)
+        {
+            Stok = newStok;
+        }
+
+        // constructor tanpa stok
+        public MenuItem(string _namaMenu , decimal _harga)
+        {
+            this.NamaMenu = _namaMenu;
+            this.Harga = _harga;
+            this.Stok = 0;
+        }
+
+        // constructor dengan stok
+        public MenuItem(string _namaMenu, decimal _harga, int _stok)
+        {
+            this.NamaMenu = _namaMenu;
+            this.Harga = _harga;
+            this.Stok = _stok;
+        }
+
+        // polymorphism
         public virtual void DisplayInfo()
         {
-            Console.WriteLine(NamaMenu + " - Rp " + Harga);
+            Console.WriteLine("Menu : " + NamaMenu + " - Rp " + Harga);
         }
     }
 }
